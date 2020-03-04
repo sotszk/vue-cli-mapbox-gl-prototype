d5cf6f507ab86d9758a7c5ff163b860017c7b309
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-03-04 18:20:40 +0900

    test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -8,6 +8,6 @@ describe('HelloWorld.vue', () => {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
-    expect(wrapper.text()).toBeTruthy();
+    // expect(wrapper.text()).toBeTruthy();
   });
 });
